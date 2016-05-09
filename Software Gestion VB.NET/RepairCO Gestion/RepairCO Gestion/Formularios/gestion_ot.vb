316183dfff89e9d5c74c99f4bbdd1e20500577c4
kanyx <grimmeychile@gmail.com>
2016-05-09 09:41:59 -0400

    fix urgente
    cambio de visualizacion urgene

--- Software Gestion VB.NET/RepairCO Gestion/RepairCO Gestion/Formularios/gestion_ot.vb
+++ Software Gestion VB.NET/RepairCO Gestion/RepairCO Gestion/Formularios/gestion_ot.vb
@@ -1,6 +1,6 @@
 ﻿Public Class gestion_ot
     Private Sub gestion_ot_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' # CARGA DE ELEMENTOS DEL FORMULARIO.
-        Me.BackgroundImage = Image.FromFile(Application.StartupPath & "/Data/grafica/frm_gestionot_background.png")
+        Me.BackColor = Color.White
     End Sub
 End Class
